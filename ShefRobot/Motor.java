88a5ec479472ecacfab7db9d79c91f84ab2d7f36
Robert Chisholm <R.Chisholm@sheffield.ac.uk>
2015-10-27 18:13:06 +0000

    Standardised the indentation and spacing (using codebeautify.org/javaviewer), purely cosmetic changes.

--- ShefRobot/Motor.java
+++ ShefRobot/Motor.java
@@ -8,102 +8,102 @@ import java.rmi.*;
 public class Motor extends PortManager {
 
     public enum Port {
-	A,B,C,D
+        A, B, C, D
     }
     public enum Type {
-	M,L
+        M, L
     }
 
     private RMIRegulatedMotor motor;
     private Port port;
     private Type type;
     Robot parentRobot;
-    
-    /** This creates a Motor object that is attached to the specified {@link Robot}, of the specified type
-	on the specified port.
-	
-	Using {@link Robot#getMotor} is preferred, since that will keep track of multiple attempts to access
-	the same motor port.
 
-	@param robot The Robot to which the motor is attached.
-	@param port The port to which the motor is attached.
-	@param type The type of motor.
+    /** This creates a Motor object that is attached to the specified {@link Robot}, of the specified type
+    on the specified port.
+    
+    Using {@link Robot#getMotor} is preferred, since that will keep track of multiple attempts to access
+    the same motor port.
+
+    @param robot The Robot to which the motor is attached.
+    @param port The port to which the motor is attached.
+    @param type The type of motor.
      */
-    public Motor(Robot robot,Port port,Type type) {
-	super(Thread.currentThread());
-	this.parentRobot = robot;
-	this.port = port;
-	this.type = type;
-	makeMotor();
+    public Motor(Robot robot, Port port, Type type) {
+        super(Thread.currentThread());
+        this.parentRobot = robot;
+        this.port = port;
+        this.type = type;
+        makeMotor();
     }
 
     /** Closes and cleans up the connection if persistent. 
 
-	<span style="font-weight: bold;">If you created this sensor with {@link Robot#getMotor}, 
-	then you should use the {@link Robot#closeMotor} method instead, 
-	otherwise the {@link Robot} object could become inconsistent. </span>
+    <span style="font-weight: bold;">If you created this sensor with {@link Robot#getMotor}, 
+    then you should use the {@link Robot#closeMotor} method instead, 
+    otherwise the {@link Robot} object could become inconsistent. </span>
      */
     public void close() {
-	this.kill();
-	if(this.motor != null) {
-	    try {
-		this.motor.close();
-	    } catch(RemoteException e) {
-		throw new RuntimeException(e);
-	    }
-	    this.motor = null;
-	}
-	// Clean up the Robot object's list, but this *shouldn't* cause an endless loop
-	// since the parent will only call this function if this isn't in the list...
-	// (it will go round twice, but that's the best way to be sure both objects agree!)
-	this.parentRobot.closeMotor(this.port);
+        this.kill();
+        if (this.motor != null) {
+            try {
+                this.motor.close();
+            } catch (RemoteException e) {
+                throw new RuntimeException(e);
+            }
+            this.motor = null;
+        }
+        // Clean up the Robot object's list, but this *shouldn't* cause an endless loop
+        // since the parent will only call this function if this isn't in the list...
+        // (it will go round twice, but that's the best way to be sure both objects agree!)
+        this.parentRobot.closeMotor(this.port);
     }
 
     /** Starts the motor rotating forwards. */
     public void forward() {
-	this.addAction("FORWARD");
+        this.addAction("FORWARD");
     }
 
     /** Starts the motor rotating backwards. */
     public void backward() {
-	this.addAction("BACKWARD");
+        this.addAction("BACKWARD");
     }
 
     /** Stops the motor. */
     public void stop() {
-	this.addAction("STOP");
+        this.addAction("STOP");
     }
 
     private void makeMotor() {
-	try {
-	    this.motor = this.parentRobot.getEV3().createRegulatedMotor(this.port.name(),this.type.name().charAt(0));
-	} catch (lejos.hardware.DeviceException e) {
-	    System.err.println("Failed to open the motor port. The most likely reason is that the previous program failed to shut down correctly and free the port. You will have to restart the EV3. Sorry :(");
-	    throw new RuntimeException("Failed to open Motor port " + this.port.name());
-	}
+        try {
+            this.motor = this.parentRobot.getEV3().createRegulatedMotor(this.port.name(), this.type.name().charAt(0));
+        } catch (lejos.hardware.DeviceException e) {
+            System.err.println("Failed to open the motor port. The most likely reason is that the previous program failed to shut down correctly and free the port. You will have to restart the EV3. Sorry :(");
+            throw new RuntimeException("Failed to open Motor port " + this.port.name());
+        }
     }
 
     protected void action(String act) {
-	if(this.motor == null) {
-	    makeMotor();
-	}
-	try {
-	    switch(act) {
-	    case "FORWARD":
-		this.motor.forward();
-		break;
-	    case "BACKWARD":
-		this.motor.backward();
-		break;
-	    case "STOP":
-		this.motor.stop(true);
-		break;
-	    default:
-		System.err.println("[" + this.port.name() + "] Asked for Action: " + act + " on a Motor...");
-	    }
-	} catch(RemoteException e) {
-	    throw new RuntimeException(e);
-	}
+        if (this.motor == null) {
+            makeMotor();
+        }
+        try {
+            switch (act) {
+                case "FORWARD":
+                    this.motor.forward();
+                    break;
+                case "BACKWARD":
+                    this.motor.backward();
+                    break;
+                case "STOP":
+                    this.motor.stop(true);
+                    break;
+                default:
+                    System.err.println("[" + this.port.name() + "] Asked for Action: " + act + " on a Motor...");
+            }
+        } catch (RemoteException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- ShefRobot/Robot.java
+++ ShefRobot/Robot.java
@@ -14,131 +14,127 @@ public class Robot {
 
     private RemoteEV3 ev3;
 
-    private HashMap<Motor.Port,Motor> motors;
-    private HashMap<Sensor.Port,Sensor> sensors;
-    
+    private HashMap <Motor.Port, Motor> motors;
+    private HashMap <Sensor.Port, Sensor> sensors;
+
     /** Create a new Robot object.
 
-	This will find the first available EV3 on the local network or Bluetooth. 
+    This will find the first available EV3 on the local network or Bluetooth. 
      */
     public Robot() {
-	try {
-	    BrickInfo[] bricks = BrickFinder.discover();
-	    if(bricks.length > 0) {
-		for(BrickInfo info: bricks) {
-		    System.out.println("EV3 found on ip: " + info.getIPAddress());
-		}
-		String ip = bricks[0].getIPAddress();
-		setup(ip);
-	    } else {
-		System.out.println("No EV3s found - check your network/bluetooth connections!");
-		throw new RuntimeException("No EV3 found.");
-	    }
-	} catch(java.net.SocketTimeoutException e) {
-	    System.err.println("Failed to find a, EV3 - have you checked your network/bluetooth connection??");
-	    throw new RuntimeException("Failed to find a Robot");
-       	} catch(Exception e) {
-	    throw new RuntimeException(e);
-	}
+        try {
+            BrickInfo[] bricks = BrickFinder.discover();
+            if (bricks.length > 0) {
+                for (BrickInfo info: bricks) {
+                    System.out.println("EV3 found on ip: " + info.getIPAddress());
+                }
+                String ip = bricks[0].getIPAddress();
+                setup(ip);
+            } else {
+                System.out.println("No EV3s found - check your network/bluetooth connections!");
+                throw new RuntimeException("No EV3 found.");
+            }
+        } catch (java.net.SocketTimeoutException e) {
+            System.err.println("Failed to find a, EV3 - have you checked your network/bluetooth connection??");
+            throw new RuntimeException("Failed to find a Robot");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /** Create a new Robot object with a specific IP address.
 
-	@param ip This defines the IP address of the Robot.
+    @param ip This defines the IP address of the Robot.
      */
     public Robot(String ip) {
-	setup(ip);
+        setup(ip);
     }
 
     /** Get a Motor object attached to the specified port.
 
-	If a Motor is already attached to this port then this function will retern a reference
-	to that Motor object, otherwise a new object is created.
+    If a Motor is already attached to this port then this function will retern a reference
+    to that Motor object, otherwise a new object is created.
 
-	@param port The port to which the motor is connected. Must be from {@link Motor.Port}.
-	@param type The type of motor. Must be from {@link Motor.Type}.
-	@return The Motor object. 
+    @param port The port to which the motor is connected. Must be from {@link Motor.Port}.
+    @param type The type of motor. Must be from {@link Motor.Type}.
+    @return The Motor object. 
      */
-    public Motor getMotor(Motor.Port port,Motor.Type type) {
-	Motor m;
-	m = this.motors.get(port);
-	if(m == null) {
-	    m = new Motor(this,port,type);
-	    this.motors.put(port,m);
-	}
-	return m;
+    public Motor getMotor(Motor.Port port, Motor.Type type) {
+        Motor m;
+        m = this.motors.get(port);
+        if (m == null) {
+            m = new Motor(this, port, type);
+            this.motors.put(port, m);
+        }
+        return m;
     }
 
     /** Get a Sensor object attached to the specified port.
 
-	 If a Sensor is already attached to this port then this function will retern a reference
-	 to that Sensor object, otherwise a new object is created.
+     If a Sensor is already attached to this port then this function will retern a reference
+     to that Sensor object, otherwise a new object is created.
 
-	@param port The port to which the motor is connected. Must be from {@link Sensor.Port}.
-	@param type The type of sensor. Must be from {@link Sensor.Type}.
-	@return The Sensor object.
+    @param port The port to which the motor is connected. Must be from {@link Sensor.Port}.
+    @param type The type of sensor. Must be from {@link Sensor.Type}.
+    @return The Sensor object.
      */
 
     public Sensor getSensor(Sensor.Port port, Sensor.Type type) {
-	Sensor s;
-	s = this.sensors.get(port);
-	if(s == null) {
-	    s = new Sensor(this,port,type);
-	    this.sensors.put(port,s);
-	}
-	return s;
+        Sensor s;
+        s = this.sensors.get(port);
+        if (s == null) {
+            s = new Sensor(this, port, type);
+            this.sensors.put(port, s);
+        }
+        return s;
     }
 
-    /** Close a Robot's connections.	
+    /** Close a Robot's connections.    
      */
     public void close() {
-	// Clone the maps to avoid concurrent modification problems.
-	HashMap<Motor.Port,Motor> ms = new HashMap<Motor.Port,Motor>(this.motors);
-	for(Motor.Port p : ms.keySet()) {
-	    this.closeMotor(p);
-	    Motor m = ms.get(p);
-	    m.close();
-	    try {
-		m.getThread().join();
-	    } catch (InterruptedException e) {
-		;
-	    }
-	}
-	HashMap<Sensor.Port,Sensor> ss = new HashMap<Sensor.Port,Sensor>(this.sensors);
-	for(Sensor.Port p : ss.keySet()) {
-	    Sensor s = this.sensors.get(p);
-	    s.close();
-	    try {
-		s.getThread().join();
-	    } catch (InterruptedException e) {
-		;
-	    }
-	}
-	this.sensors = new HashMap<Sensor.Port,Sensor>();
+        // Clone the maps to avoid concurrent modification problems.
+        HashMap < Motor.Port, Motor > ms = new HashMap < Motor.Port, Motor > (this.motors);
+        for (Motor.Port p: ms.keySet()) {
+            this.closeMotor(p);
+            Motor m = ms.get(p);
+            m.close();
+            try {
+                m.getThread().join();
+            } catch (InterruptedException e) {}
+        }
+        HashMap < Sensor.Port, Sensor > ss = new HashMap < Sensor.Port, Sensor > (this.sensors);
+        for (Sensor.Port p: ss.keySet()) {
+            Sensor s = this.sensors.get(p);
+            s.close();
+            try {
+                s.getThread().join();
+            } catch (InterruptedException e) {}
+        }
+        this.sensors = new HashMap < Sensor.Port, Sensor > ();
     }
-    
+
     /** This method allows you to close one Motor connection. 
 
-	@param port The port to which the motor is connected.
+    @param port The port to which the motor is connected.
     */
     public void closeMotor(Motor.Port port) {
-	Motor m = this.motors.get(port);
-	if(m != null) {
-	    this.motors.remove(port);
-	    m.close();
-	}
+        Motor m = this.motors.get(port);
+        if (m != null) {
+            this.motors.remove(port);
+            m.close();
+        }
     }
 
     /** This method allows you to close one Sensor connection. 
 
-	@param port The port to which the sensor is connected.
+    @param port The port to which the sensor is connected.
      */
     public void closeSensor(Sensor.Port port) {
-	Sensor s = this.sensors.get(port);
-	if(s != null) {
-	    this.sensors.remove(port);
-	    s.close();
-	}
+        Sensor s = this.sensors.get(port);
+        if (s != null) {
+            this.sensors.remove(port);
+            s.close();
+        }
     }
 
     /** This is a utility method that is used by the {@link Motor Motors} and {@link Sensor Sensors} to 
@@ -147,18 +143,18 @@ public class Robot {
      @return The RemoteEV3 object from the LeJOS package.
     */
     protected RemoteEV3 getEV3() {
-	return this.ev3;
+        return this.ev3;
     }
 
     // Centralised setup method that is called by the constructors after finding IPs etc.
     private void setup(String ip) {
-	this.motors = new HashMap<Motor.Port,Motor>();
-	this.sensors = new HashMap<Sensor.Port,Sensor>();
-	try {
-	    this.ev3 = new RemoteEV3(ip);
-	} catch(Exception e) {
-	    throw new RuntimeException(e);
-	}
+        this.motors = new HashMap <Motor.Port, Motor> ();
+        this.sensors = new HashMap <Sensor.Port, Sensor> ();
+        try {
+            this.ev3 = new RemoteEV3(ip);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
